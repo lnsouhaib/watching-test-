930178968fc8f78a4f2ed9d75645fc7909a06212
lnsouhaib <50969948+lnsouhaib@users.noreply.github.com>
2019-05-24 16:41:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var prefix = "+";
 var adminprefix = '#'
 
 
-const developers = ["ID","ID"]
+const developers = ["410860706213134336"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
